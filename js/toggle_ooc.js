7c44a8263837fc394e569c9bc3ac75ee9f8093b3
anrodger <me@andrew.rs>
2015-03-21 20:07:10 -0400

    Restyling the OOC toggle for mobile

--- js/toggle_ooc.js
+++ js/toggle_ooc.js
@@ -1,18 +1,61 @@
 function toggleOOC() {
-	// var topPost = $("p.rp").withinViewportTop().first();
-	// var topPost = $("p.rp").not(":above-the-top").first();
-	var originalScroll = $(window).scrollTop();
-	var topPost = $("p.rp").filter(function(index, elem) {
-		return $(elem).offset().top >= originalScroll;
-	}).first();
+    // var topPost = $("p.rp").withinViewportTop().first();
+    // var topPost = $("p.rp").not(":above-the-top").first();
+    var originalScroll = $(window).scrollTop();
+    var topPost = $("p.rp").filter(function(index, elem) {
+        return $(elem).offset().top >= originalScroll;
+    }).first();
 
-	var originalOffset = topPost.offset();
+    var originalOffset = topPost.offset();
 
-	// checked == show; not checked == hide
-	var visible = $("#ooc_toggle").is(":checked");
-	$(".ooc").toggle(visible);
+    // checked == show; not checked == hide
+    var visible = $("#ooc_toggle").is(":checked");
+    $(".ooc").toggle(visible);
 
-	$(window).scrollTop(originalScroll + (topPost.offset().top - originalOffset.top));
+    $(window).scrollTop(originalScroll + (topPost.offset().top - originalOffset.top));
 }
+
+(function() {
+    var oocButton = $("#ooc_label");
+    //  get window
+    // use vars to short circuit doing work again
+    var shown = false;
+    var timeout;
+    var lastOffset = 0;
+
+    var resetHideTimer = function() {
+        clearTimeout(timeout);
+        timeout = setTimeout(function() {
+            oocButton.css('opacity', 0);
+            shown = false;
+        }, 2000);
+    }
+
+    $(window).on("scroll", function() {
+        var curOffset = oocButton.offset().top;
+        if (!shown && curOffset > 200 && curOffset < lastOffset) {
+            oocButton.css('opacity', 1);
+            shown = true;
+        } else if (shown && curOffset > lastOffset) {
+            oocButton.css('opacity', 0);
+            shown = false;
+        }
+
+        lastOffset = curOffset;
+        resetHideTimer();
+    });
+
+    $(window).on("click", function() {
+        if (!shown) {
+            oocButton.css('opacity', 1);
+            shown = true;
+        } else if (shown) {
+            oocButton.css('opacity', 0);
+            shown = false;
+        }
+
+        resetHideTimer();
+    })
+})();
 
 toggleOOC();
