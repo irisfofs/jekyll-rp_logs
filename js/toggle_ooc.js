ca5a65edbb12f17230c751296f298afda6e2b184
anrodger <me@andrew.rs>
2015-03-22 02:44:17 -0400

    Refactoring and bug fixing for OOC toggle

--- js/toggle_ooc.js
+++ js/toggle_ooc.js
@@ -1,53 +1,53 @@
-function toggleOOC() {
-    // var topPost = $("p.rp").withinViewportTop().first();
-    // var topPost = $("p.rp").not(":above-the-top").first();
-    var originalScroll = $(window).scrollTop();
-    var topPost = $("p.rp").filter(function(index, elem) {
-        return $(elem).offset().top >= originalScroll;
-    }).first();
-
-    var originalOffset = topPost.offset();
-
-    // checked == show; not checked == hide
-    var visible = $("#ooc_toggle").is(":checked");
-    $(".ooc").toggle(visible);
-
-    $(window).scrollTop(originalScroll + (topPost.offset().top - originalOffset.top));
-}
-
 (function() {
     var oocButton = $("#ooc_label");
-    //  get window
+    // get window
+    var win = $(window);
     // use vars to short circuit doing work again
     var shown = false;
-    var timeout;
+    var canClose = false;
+    
     var lastScroll = 0;
     var minScroll = 200; // how far down in order to show it
+    
+    var timeout;
+    var closeableTimeout;
+
+    var hideButton = function() {
+        shown = false;
+        oocButton.removeClass('shown');
+        clearHideTimer();
+    }
+
+    var showButton = function() {
+        shown = true;
+        oocButton.addClass('shown');
+        resetHideTimer();
+    }
+
+    var allowButtonClose = function() {
+        canClose = true;
+    }
 
     var resetHideTimer = function() {
         clearTimeout(timeout);
-        timeout = setTimeout(function() {
-            oocButton.removeClass('shown');
-            shown = false;
-        }, 2000);
+        clearTimeout(closeableTimeout);
+        canClose = false;
+        timeout = setTimeout(hideButton, 2000);
+        closeableTimeout = setTimeout(allowButtonClose, 500);
     }
 
     var clearHideTimer = function() {
         clearTimeout(timeout);
     }
 
-    var win = $(window);
-
     win.on("scroll", function() {
         var curScroll = win.scrollTop();
         if (!shown && curScroll > minScroll && curScroll < lastScroll) {
-            shown = true;
-            oocButton.addClass('shown');
-            resetHideTimer();
-        } else if (shown && (curScroll > lastScroll || curScroll < minScroll)) {
-            shown = false;
-            oocButton.removeClass('shown');
-            clearTimeout();
+            showButton();
+        } else if (shown && canClose && (curScroll > lastScroll || curScroll < minScroll)) {
+            // keep it from closing early
+            // have some min time before closing
+            hideButton();
         }
 
         lastScroll = curScroll;
@@ -55,15 +55,36 @@ function toggleOOC() {
 
     win.on("click", function() {
         if (!shown) {
-            shown = true;
-            oocButton.addClass('shown');
-            resetHideTimer();
+            showButton();
         } else if (shown) {
-            shown = false;
-            oocButton.removeClass('shown');
-            clearHideTimer();
+            hideButton();
         }
-    })
+    });
+
+    $("#ooc_label").click(function(e) {
+        // Keep it from getting to the main window and hiding the label.
+        e.stopPropagation();
+        resetHideTimer();
+    });
+
+    var toggleOOC = function(e) {
+        // var topPost = $("p.rp").withinViewportTop().first();
+        // var topPost = $("p.rp").not(":above-the-top").first();
+        var originalScroll = $(window).scrollTop();
+        var topPost = $("p.rp").filter(function(index, elem) {
+            return $(elem).offset().top >= originalScroll;
+        }).first();
+
+        var originalOffset = topPost.offset();
+
+        // checked == show; not checked == hide
+        var visible = $("#ooc_toggle").is(":checked");
+        $(".ooc").toggle(visible);
+
+        $(window).scrollTop(originalScroll + (topPost.offset().top - originalOffset.top));
+    }
+    toggleOOC();
+
+    $("#ooc_toggle").change(toggleOOC)
 })();
 
-toggleOOC();
