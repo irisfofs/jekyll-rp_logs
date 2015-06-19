30cf5d2ea6b078e48d229800d40b9ad368de8beb
derpynirvash <nirvash@derpymail.org>
2015-06-19 10:59:04 -0500

    parser for skype logs, copy pasted out of the program

---
+++ _plugins/parse_zskype.rb
@@ -0,0 +1,50 @@
+require_relative "rp_log_converter"
+require_relative "rp_parser"
+
+module RpLogs
+
+  class ZskypeParser < RpLogs::Parser
+
+    # Add this class to the parsing dictionary
+    FORMAT_STR = 'Zskype'
+    RpLogGenerator.add self
+
+    # Stuff
+    class << self
+      NICK = /([\w\-\\\[\]\{\}\^\`\|\s\']+)/
+      DATE_REGEXP = /(\[\d\d.\d\d.\d\d\s\d\d\:\d\d\:\d\d\])/ 
+      FLAGS = /((?:![A-Z]+ )*)/
+	  
+	 # Crappy but works
+	  USER_AT_HOST = /\(\w+@[^)]+\)/
+#Not needed?	  JUNK = /#{DATE_REGEXP} \* #{MODE}#{NICK} (sets mode:|is now known as|(#{USER_AT_HOST} (has joined|Quit|has left))).*$/
+      EMOTE = /^#{FLAGS}#{DATE_REGEXP}\s#{NICK}:\s(\3)([^\n]*)$/
+      TEXT  = /^#{FLAGS}#{DATE_REGEXP}\s#{NICK}:\s([^\n]*)$/
+
+      TIMESTAMP_FORMAT = '[%d.%m.%y %H:%M:%S]'
+
+      def parse_line(line, options = {}) 
+        case line
+       # when JUNK
+         # nil
+        when EMOTE
+		
+          date = DateTime.strptime($2, TIMESTAMP_FORMAT)
+		  sendername = $3.tr(' ', '-').tr("'", "")
+          Parser::LogLine.new(date, options, sender: sendername, contents: $5, \
+            flags: $1, type: :rp)
+        when TEXT
+          date = DateTime.strptime($2, TIMESTAMP_FORMAT)
+		  sendername = $3.tr(' ', '-').tr("'", "")
+          Parser::LogLine.new(date, options, sender: sendername, contents: $4, \
+            flags: $1, type: :ooc)
+        else
+          # Only put text and emotes in the log
+          nil
+        end
+      end
+    end
+
+  end  
+
+end
