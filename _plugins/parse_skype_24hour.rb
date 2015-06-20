8e82ac043ed18fee06cc22ff32447443427cd5c4
derpynirvash <nirvash@derpymail.org>
2015-06-19 22:58:58 -0500

    Added in usename filters

--- _plugins/parse_skype_24hour.rb
+++ _plugins/parse_skype_24hour.rb
@@ -14,7 +14,7 @@ module RpLogs
       NICK = /([\w\-\\\[\]\{\}\^\`\|\s\']+)/
       DATE_REGEXP = /(\[\d\d.\d\d.\d\d\s\d\d\:\d\d\:\d\d\])/ 
       FLAGS = /((?:![A-Z]+ )*)/
-	  
+	  BAD_STUFF = /[^a-zA-Z\-\_]/
 	 # Crappy but works
 	  USER_AT_HOST = /\(\w+@[^)]+\)/
 #Not needed?	  JUNK = /#{DATE_REGEXP} \* #{MODE}#{NICK} (sets mode:|is now known as|(#{USER_AT_HOST} (has joined|Quit|has left))).*$/
@@ -28,16 +28,19 @@ module RpLogs
        # when JUNK
          # nil
         when EMOTE
-		
           date = DateTime.strptime($2, TIMESTAMP_FORMAT)
-		  sendername = $3.tr(' ', '-').tr("'", "")
-          Parser::LogLine.new(date, options, sender: sendername, contents: $5, \
-            flags: $1, type: :rp)
+		  contents = $5
+		  flags = $1
+		  sendername = $3.tr(' ', '-').gsub(BAD_STUFF, "")
+          Parser::LogLine.new(date, options, sender: sendername, contents: contents, \
+          flags: flags, type: :rp)
         when TEXT
           date = DateTime.strptime($2, TIMESTAMP_FORMAT)
-		  sendername = $3.tr(' ', '-').tr("'", "")
-          Parser::LogLine.new(date, options, sender: sendername, contents: $4, \
-            flags: $1, type: :ooc)
+		  contents = $4
+		  flags = $1
+		  sendername = $3.tr(' ', '-').gsub(BAD_STUFF, "")
+          Parser::LogLine.new(date, options, sender: sendername, contents: contents, \
+            flags: flags, type: :ooc)
         else
           # Only put text and emotes in the log
           nil
