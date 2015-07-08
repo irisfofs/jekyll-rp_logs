078df5b1b3308a33a61dc21bdc80f0c2c8b44b32
anrodger <me@andrew.rs>
2015-07-08 04:15:26 -0400

    Converted tabs to spaces and fixed indentation

--- lib/jekyll/rp_logs/parse_skype_12hour.rb
+++ lib/jekyll/rp_logs/parse_skype_12hour.rb
@@ -12,11 +12,11 @@ module Jekyll
         NICK = /([\w\-\\\[\]\{\}\^\`\|\s\'\)\(]+)/
         DATE_REGEXP = /(\[\d?\d\/\d?\d\/\d\d\d\d\s\d?\d\:\d\d\:\d\d\s(AM|PM)\])/ 
         FLAGS = /((?:![A-Z]+ )*)/
-  	  BAD_STUFF = /[^a-zA-Z\-\_]/
-  	  
-  	 # Crappy but works
-  	  USER_AT_HOST = /\(\w+@[^)]+\)/
-  #Not needed?	  JUNK = /#{DATE_REGEXP} \* #{MODE}#{NICK} (sets mode:|is now known as|(#{USER_AT_HOST} (has joined|Quit|has left))).*$/
+        BAD_STUFF = /[^a-zA-Z\-\_]/
+      
+        # Crappy but works
+        USER_AT_HOST = /\(\w+@[^)]+\)/
+        #Not needed?    JUNK = /#{DATE_REGEXP} \* #{MODE}#{NICK} (sets mode:|is now known as|(#{USER_AT_HOST} (has joined|Quit|has left))).*$/
         EMOTE = /^#{FLAGS}#{DATE_REGEXP}\s#{NICK}:\s(\4)([^\n]*)$/
         TEXT  = /^#{FLAGS}#{DATE_REGEXP}\s#{NICK}:\s([^\n]*)$/
 
@@ -27,24 +27,24 @@ module Jekyll
           #when JUNK
             #nil
           when EMOTE
-  		print "1"
+            print "1"
             date = DateTime.strptime($2, TIMESTAMP_FORMAT)
             contents = $6
-  		  flags = $1
-  		  sendername = $4.tr(' ', '-').gsub(BAD_STUFF, "")
+            flags = $1
+            sendername = $4.tr(' ', '-').gsub(BAD_STUFF, "")
             Parser::LogLine.new(date, options, sender: sendername, contents: contents, \
             flags: flags, type: :rp)
           when TEXT
-  		print "2"
+            # print "2"
             date = DateTime.strptime($2, TIMESTAMP_FORMAT)
-  		  contents = $5
-  		  flags = $1
-  		  sendername = $4.tr(' ', '-').gsub(BAD_STUFF, "")
+            contents = $5
+            flags = $1
+            sendername = $4.tr(' ', '-').gsub(BAD_STUFF, "")
             Parser::LogLine.new(date, options, sender: sendername, contents: contents, \
               flags: flags, type: :ooc)
           else
-            print "3"
-  		  # Only put text and emotes in the log
+            # print "3"
+            # Only put text and emotes in the log
             nil
           end
         end
--- lib/jekyll/rp_logs/parse_skype_24hour.rb
+++ lib/jekyll/rp_logs/parse_skype_24hour.rb
@@ -12,10 +12,10 @@ module Jekyll
         NICK = /([\w\-\\\[\]\{\}\^\`\|\s\']+)/
         DATE_REGEXP = /(\[\d\d.\d\d.\d\d\s\d\d\:\d\d\:\d\d\])/ 
         FLAGS = /((?:![A-Z]+ )*)/
-  	  BAD_STUFF = /[^a-zA-Z\-\_]/
-  	 # Crappy but works
-  	  USER_AT_HOST = /\(\w+@[^)]+\)/
-  #Not needed?	  JUNK = /#{DATE_REGEXP} \* #{MODE}#{NICK} (sets mode:|is now known as|(#{USER_AT_HOST} (has joined|Quit|has left))).*$/
+        BAD_STUFF = /[^a-zA-Z\-\_]/
+        # Crappy but works
+        USER_AT_HOST = /\(\w+@[^)]+\)/
+        #Not needed?    JUNK = /#{DATE_REGEXP} \* #{MODE}#{NICK} (sets mode:|is now known as|(#{USER_AT_HOST} (has joined|Quit|has left))).*$/
         EMOTE = /^#{FLAGS}#{DATE_REGEXP}\s#{NICK}:\s(\3)([^\n]*)$/
         TEXT  = /^#{FLAGS}#{DATE_REGEXP}\s#{NICK}:\s([^\n]*)$/
 
@@ -27,16 +27,16 @@ module Jekyll
            # nil
           when EMOTE
             date = DateTime.strptime($2, TIMESTAMP_FORMAT)
-  		  contents = $5
-  		  flags = $1
-  		  sendername = $3.tr(' ', '-').gsub(BAD_STUFF, "")
+            contents = $5
+            flags = $1
+            sendername = $3.tr(' ', '-').gsub(BAD_STUFF, "")
             Parser::LogLine.new(date, options, sender: sendername, contents: contents, \
             flags: flags, type: :rp)
           when TEXT
             date = DateTime.strptime($2, TIMESTAMP_FORMAT)
-  		  contents = $4
-  		  flags = $1
-  		  sendername = $3.tr(' ', '-').gsub(BAD_STUFF, "")
+            contents = $4
+            flags = $1
+            sendername = $3.tr(' ', '-').gsub(BAD_STUFF, "")
             Parser::LogLine.new(date, options, sender: sendername, contents: contents, \
               flags: flags, type: :ooc)
           else
