14b027f1e79453edfdbd1e7e6663dcbb07ceb84e
anrodger <me@andrew.rs>
2016-01-29 17:09:51 -0500

    `rplogs update` checks for wrong directory
    When a `_config.yml` isn't found, it prompts the user to continue and
    informs them that this may not be a Jekyll site directory.
    
    It still prompts for overwriting when a `_sass` directory exists.
    
    Also reorganized the location of the gem-provided rake tasks so they
    weren't nested three levels deep inside a class.
    
    Closes #69.

--- lib/jekyll/rp_logs/rp_tasks.rb
+++ lib/jekyll/rp_logs/rp_tasks.rb
@@ -2,85 +2,13 @@ require "rake"
 
 module Jekyll
   module RpLogs
+    ##
+    # Loads the gem's built-in rake tasks from tasks/rp_logs.rake
     class RpTasks
       include Rake::DSL if defined? Rake::DSL
 
-      def copy_unless_exist(from, to, message = nil)
-        unless File.exist?(to)
-          puts message if message
-          cp from, to
-        end
-      end
-
-      # Octopress
-      def get_stdin(message)
-        print message
-        STDIN.gets.chomp
-      end
-
-      # Octopress
-      def ask(message, valid_options)
-        if valid_options
-          answer = get_stdin("#{message} #{valid_options.to_s.gsub(/"/, '').gsub(/, /, '/')} ") until valid_options.include?(answer)
-        else
-          answer = get_stdin(message)
-        end
-        answer
-      end
-
       def install_tasks
-        namespace :rp_logs do
-          desc "Create a new Jekyll site for RP logs, with the default theme"
-          task :create_new_site, [:dir] do |t, args|
-            if Dir.exist? args[:dir]
-              if (Dir.entries(args[:dir]) - %w(. ..)).empty?
-                puts "Using empty directory #{args[:dir]}"
-              else
-                fail "Directory #{args[:dir]} already exists, and is non-empty. Won't overwrite."
-              end
-            else
-              mkdir_p args[:dir]
-            end
-
-            Dir.chdir(args[:dir]) do
-              Rake::Task["rp_logs:bundler"].invoke
-              Rake::Task["rp_logs:copy_theme"].invoke
-            end
-          end
-
-          directory "_rps"
-
-          file "Gemfile" do |tsk|
-            File.open(tsk.name, "w") do |f|
-              f << <<-END.gsub(/^\s+\|/, "")
-                |source "https://rubygems.org"
-                |
-                |group :jekyll_plugins do
-                |  gem "jekyll-rp_logs"
-                |end
-              END
-            end
-          end
-
-          task bundler: "Gemfile" do
-            sh "bundle"
-          end
-
-          task :copy_theme do
-            if File.directory?("_sass")
-              abort("rake aborted!") if ask("A theme is already installed, proceeding will overwrite existing non-custom files. Are you sure?", ['y', 'n']) == 'n'
-            end
-
-            Rake::Task[:_rps].invoke
-            # allow directory specification
-
-            gem_root = Gem::Specification.find_by_name("jekyll-rp_logs").gem_dir
-            cp_r Dir["#{gem_root}/.themes/default/source/*"], "./"
-            copy_unless_exist("_config.yml.default", "_config.yml")
-            touch "_sass/_custom-vars.scss"
-            touch "_sass/_custom-rules.scss"
-          end
-        end
+        load "tasks/rp_logs.rake"
       end
     end
   end
---
+++ lib/tasks/rp_logs.rake
@@ -0,0 +1,82 @@
+Y_OR_N = %w(y n)
+NO = "n"
+
+def copy_unless_exist(from, to, message = nil)
+  return if File.exist?(to)
+  puts message if message
+  cp from, to
+end
+
+# Octopress
+def get_stdin(message)
+  print message
+  STDIN.gets.chomp
+end
+
+# Octopress
+def ask(message, valid_options = Y_OR_N)
+  if valid_options
+    valid_str = valid_options.to_s.delete('"').gsub(/, /, "/")
+    answer = get_stdin("#{message} #{valid_str} ") until valid_options.include?(answer)
+  else
+    answer = get_stdin(message)
+  end
+  answer
+end
+
+namespace :rp_logs do
+  desc "Create a new Jekyll site for RP logs, with the default theme"
+  task :create_new_site, [:dir] => [:site_dir] do |_task, args|
+    Dir.chdir(args[:dir]) do
+      Rake::Task["rp_logs:bundler"].invoke
+      Rake::Task["rp_logs:copy_theme"].invoke
+    end
+  end
+
+  task :site_dir, [:dir] do |_task, args|
+    if Dir.exist? args[:dir]
+      if (Dir.entries(args[:dir]) - %w(. ..)).empty?
+        puts "Using empty directory #{args[:dir]}"
+      else
+        fail "Directory #{args[:dir]} already exists, and is non-empty. Won't overwrite."
+      end
+    else
+      mkdir_p args[:dir]
+    end
+  end
+
+  directory "_rps"
+
+  file "Gemfile" do |tsk|
+    File.open(tsk.name, "w") do |f|
+      f << <<-END.gsub(/^\s+\|/, "")
+        |source "https://rubygems.org"
+        |
+        |group :jekyll_plugins do
+        |  gem "jekyll-rp_logs"
+        |end
+      END
+    end
+  end
+
+  task bundler: "Gemfile" do
+    sh "bundle"
+  end
+
+  task copy_theme: :_rps do
+    gem_root = Gem::Specification.find_by_name("jekyll-rp_logs").gem_dir
+    cp_r Dir["#{gem_root}/.themes/default/source/*"], "./"
+    copy_unless_exist("_config.yml.default", "_config.yml")
+    touch "_sass/_custom-vars.scss"
+    touch "_sass/_custom-rules.scss"
+  end
+
+  task :update_theme do
+    if !File.exist?("_config.yml")
+      abort("Didn't install theme.") if ask("No _config.yml found. This may not be a Jekyll site directory. Install theme anyway?") == NO
+    elsif File.exist?("_sass")
+      abort("Kept existing theme.") if ask("A theme is already installed. Overwrite existing non-custom files?") == NO
+    end
+    Rake::Task["rp_logs:copy_theme"].invoke
+  end
+end
