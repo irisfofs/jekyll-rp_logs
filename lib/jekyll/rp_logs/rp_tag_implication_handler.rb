0aea4dbd588aaa4fe37a8355e93664f124b8e595
anrodger <me@andrew.rs>
2016-01-31 13:30:07 -0500

    Improve TagImplicationHandler methods
    Added a helper method to replace the duplicate looping behavior in both
    implicate_tags and alias_tags.
    
    Moved the failure message for cyclical tags to its own method, and made
    alias_tags call it directly instead of throwing back to update_tags.
    
    This is part of work on #71.

--- lib/jekyll/rp_logs/rp_tag_implication_handler.rb
+++ lib/jekyll/rp_logs/rp_tag_implication_handler.rb
@@ -1,5 +1,9 @@
 module Jekyll
   module RpLogs
+    ##
+    # A `TagImplicationHandler` takes a set of tag implications and tag aliases
+    # from a Jekyll config file and provides methods to update a set of tags to
+    # include all of its (recursively) implicated and aliased tags.
     class TagImplicationHandler
       class TagImplicationError < StandardError
       end
@@ -14,18 +18,17 @@ module Jekyll
         validate_tag_rules
       end
 
+      ##
+      # Iteratively apply tag implications and tag aliases until the list of
+      # tags stops changing.
       def update_tags(tag_set, verbose: false)
         removed_tags = Set.new
         loop do
           previous_tags = tag_set.clone
-          cyclical = catch :cyclical_tags do
-            implicate_tags(tag_set, removed_tags, verbose)
-            alias_tags(tag_set, removed_tags)
-            false
-          end
-          if cyclical
-            fail_with "The tag #{cyclical[1]} (from #{cyclical[0]} => #{cyclical[1]}) has been removed before. There is a cycle in the tag aliases and implications."
-          end
+
+          implicate_tags(tag_set, removed_tags, verbose)
+          alias_tags(tag_set, removed_tags)
+
           # Break when there is no change in tags.
           return tag_set if tag_set == previous_tags
         end
@@ -40,7 +43,8 @@ module Jekyll
         # Check for aliases and implications from the same tag
         dupe = @tag_implications.keys.find { |k| @tag_aliases.key? k }
         if dupe
-          fail_with "Tag \"#{dupe}\" is both aliased and implied from. Imply from the alias instead."
+          fail_with "Tag \"#{dupe}\" is both aliased and implied from. "\
+                    "Imply from the alias instead."
         end
 
         error_for_aliases_that_should_be_implications
@@ -56,49 +60,59 @@ module Jekyll
       # number of tag implications defined, and implications only add tags.
       # They can't remove tags.
       def implicate_tags(tag_set, removed_tags, verbose)
-        tags_to_check = tag_set
-        loop do
-          # Because we use this set again as the tags to check we don't want
-          # to clear it.
-          to_add = Set.new
-          tags_to_check.each do |tag|
-            imply = @tag_implications.fetch(tag, [])
+        until_tags_stabilize(tag_set) do |tag, to_add|
+          imply = @tag_implications.fetch(tag, [])
 
-            removed, imply = imply.partition { |t| removed_tags.include? t }
-            # It's okay if we want to imply a removed tag. Maybe?
-            if verbose && !removed.empty?
-              string = removed.size == 1 ? "is an aliased tag" : "are aliased tags"
-              Jekyll.logger.warn "#{tag} implies #{removed}, which #{string}. Consider implying the alised tag directly."
-            end
-
-            to_add.merge imply
+          removed, imply = imply.partition { |t| removed_tags.include? t }
+          # It's okay if we want to imply a removed tag. Maybe?
+          if verbose && !removed.empty?
+            string = removed.size == 1 ? "is an aliased tag" : "are aliased tags"
+            Jekyll.logger.warn "#{tag} implies #{removed}, which #{string}. Consider implying "\
+                               "the alised tag directly."
           end
 
-          break if to_add.empty?
-          tag_set.merge to_add
-          tags_to_check = to_add
+          to_add.merge imply
         end
       end
 
       ##
       # Iteratively apply tag aliases until no more can be applied
       def alias_tags(tag_set, removed_tags)
+        until_tags_stabilize(tag_set) do |tag, to_add|
+          next unless @tag_aliases.key? tag
+          aliased = @tag_aliases[tag]
+
+          # If we are trying to alias back a tag already removed, there is a
+          # cycle in the tag aliases and implications.
+          removed = aliased.find { |t| removed_tags.include? t }
+          error_for_cyclical_tags(tag, removed) if removed
+
+          # if it's already in the set, something weird happened
+          removed_tags << tag
+          tag_set.delete tag
+          to_add.merge aliased
+        end
+      end
+
+      def error_for_cyclical_tags(tag, removed_tag)
+        fail_with "The tag #{removed_tag} (from #{tag} => #{removed_tag}) has "\
+                  "been removed before. There is a cycle in the tag aliases "\
+                  "and implications."
+      end
+
+      ##
+      # Run the given block for each tag in tag_set, included tags added by
+      # aliases or implications. If tag_set changes as a result of this, then
+      # it runs again. This continues until there are no more aliases or
+      # implications to add.
+      def until_tags_stabilize(tag_set)
         tags_to_check = tag_set
         loop do
+          # Because we use this set again as the tags to check we don't want
+          # to clear it.
           to_add = Set.new
           tags_to_check.each do |tag|
-            next unless @tag_aliases.key? tag
-
-            aliased = @tag_aliases[tag]
-            aliased.each do |t|
-              next unless removed_tags.include? t
-              throw(:cyclical_tags, [tag, t])
-            end
-
-            # if it's already in the set, something weird happened
-            removed_tags << tag
-            tag_set.delete tag
-            to_add.merge aliased
+            yield(tag, to_add)
           end
 
           break if to_add.empty?
@@ -114,7 +128,8 @@ module Jekyll
         error_messages = []
         @tag_aliases.each_pair do |k, v|
           if v.include? k
-            error_messages << "Alias #{k} => #{v} is equivalent to an implication. #{k} will not be removed."
+            error_messages << "Alias #{k} => #{v} is equivalent to an implication. "\
+                              "#{k} will not be removed."
           end
         end
         fail_with error_messages.join("\n") unless error_messages.empty?
